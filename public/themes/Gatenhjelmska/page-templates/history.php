c261cf851e20e6e6c0de2af78955e0c7a471eeaf
Andreas Pandzic <andreas.pandzic@hotmail.com>
2020-03-29 14:26:57 +0200

    file-structure

---
+++ public/themes/Gatenhjelmska/page-templates/history.php
@@ -0,0 +1,16 @@
+<?php /* Template name: History */ ?>
+
+<?php get_header(); ?>
+<section class="history">
+    <div>
+        <div>
+            <?php if (have_posts()) : ?>
+                <?php while (have_posts()) : the_post(); ?>
+                    <h1><?php the_title(); ?></h1>
+                    <?php the_content(); ?>
+                <?php endwhile; ?>
+            <?php endif; ?>
+        </div><!-- /col -->
+    </div><!-- /row -->
+</section>
+<?php get_footer(); ?>
