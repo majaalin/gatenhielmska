0174ee1a35033872265edadf6bef70bac3e3f84e
Andreas Pandzic <andreas.pandzic@hotmail.com>
2020-03-29 14:50:37 +0200

    file-structure

---
+++ public/themes/Gatenhjelmska/page-templates/news.php
@@ -0,0 +1,16 @@
+<?php /* Template name: News */ ?>
+
+<?php get_header(); ?>
+<section class="home">
+    <div>
+        <div>
+            <?php if (have_posts()) : ?>
+                <?php while (have_posts()) : the_post(); ?>
+                    <h1><?php the_title(); ?></h1>
+                    <?php the_content(); ?>
+                <?php endwhile; ?>
+            <?php endif; ?>
+        </div><!-- /col -->
+    </div><!-- /row -->
+</section>
+<?php get_footer(); ?>
