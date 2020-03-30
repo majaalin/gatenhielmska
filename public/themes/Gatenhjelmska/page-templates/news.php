8689bf61b0d07cbe5a0b32dde8a4919ccf420b7d
majaalin <maja_alin@hotmail.com>
2020-03-30 20:36:38 +0200

    Update styling for news

--- public/themes/Gatenhjelmska/page-templates/news.php
+++ public/themes/Gatenhjelmska/page-templates/news.php
@@ -16,9 +16,9 @@ if( !empty( $image ) ): ?>
     <img class="news-img" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
 <?php endif; ?>
 <div class="text-container">
-<p>Publicerad: <?php the_field('date'); ?></p>
-                    <p><?php the_title(); ?></p>
-                    <p><?php the_field('summary'); ?></p>
+<p class="date">Publicerad: <?php the_field('date'); ?></p>
+                    <h3 class="title"><?php the_title(); ?></h3>
+                    <p class="summary"><?php the_field('summary'); ?></p>
                     </div>
                 <?php endforeach; ?>
             </ul>
