2ed7ff24a70e6507c0b84dc6023b7ac30a2e3a88
majaalin <maja_alin@hotmail.com>
2020-04-01 08:34:06 +0200

    Update footer css

--- public/themes/Gatenhjelmska/footer.php
+++ public/themes/Gatenhjelmska/footer.php
@@ -1,3 +1,29 @@
   <?php wp_footer(); ?>
+  <section class="footer"> 
+  <ul>
+    <li>Länkar lista</li>
+    <li><a href="">veniam nisi</a></li>
+    <li><a href="">deserunt sint</a></li>
+    <li><a href="">ad eiusmod</a></li>
+    <li><a href="">pariatur id</a></li>
+    <li><a href="">consequat officia</a></li>
+  </ul>
+  <ul>
+    <li>Länkar lista</li>
+    <li><a href="">veniam nisi</a></li>
+    <li><a href="">deserunt sint</a></li>
+    <li><a href="">ad eiusmod</a></li>
+    <li><a href="">pariatur id</a></li>
+    <li><a href="">consequat officia</a></li>
+  </ul>
+  <ul>
+    <li>Länkar lista</li>
+    <li><a href="">veniam nisi</a></li>
+    <li><a href="">deserunt sint</a></li>
+    <li><a href="">ad eiusmod</a></li>
+    <li><a href="">pariatur id</a></li>
+    <li><a href="">consequat officia</a></li>
+  </ul>
+  </section>
 </body>
 </html>
