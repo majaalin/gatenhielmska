36c3cf25fb34bed8921668881e09a64ed23068dc
majaalin <maja_alin@hotmail.com>
2020-04-07 18:25:56 +0200

    Update menu javacript

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -2,13 +2,19 @@ window.onscroll = function() {
   scrollFunction();
 };
 
-const subMenu = document.getElementsByClassName("sub-menu");
-
 function scrollFunction() {
+  const subMenu = document.querySelectorAll(".sub-menu");
+
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.querySelector("header").style.backgroundColor =
       "rgba(13, 17, 23, 0.97)";
+    subMenu.forEach(element => {
+      element.style.backgroundColor = "rgba(13, 17, 23, 0.97)";
+    });
   } else {
     document.querySelector("header").style.backgroundColor = "transparent";
+    subMenu.forEach(element => {
+      element.style.backgroundColor = "transparent";
+    });
   }
 }
