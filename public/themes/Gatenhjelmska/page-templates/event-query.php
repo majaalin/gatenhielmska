5b61dcbad25d60a1def361e54c36a3286acd07c8
majaalin <maja_alin@hotmail.com>
2020-04-10 12:01:26 +0200

    Update css for event-query

--- public/themes/Gatenhjelmska/page-templates/event-query.php
+++ public/themes/Gatenhjelmska/page-templates/event-query.php
@@ -2,15 +2,35 @@
 
 <?php get_header(); ?>
 <section class="event-query">
-    <div>
-        <div>
+<img class="background-img" src="<?php bloginfo('template_directory') ?>/assets/images/event-query.png" alt="">
+        <div class="contact-form">
             <?php if (have_posts()) : ?>
                 <?php while (have_posts()) : the_post(); ?>
-                    <h1><?php the_title(); ?></h1>
+                    <h1><?php the_field('h1'); ?></h1>
+                    <p><?php the_field('paragraf'); ?></p>
                     <?php the_content(); ?>
-                <?php endwhile; ?>
+                    <?php endwhile; ?>
             <?php endif; ?>
-        </div><!-- /col -->
-    </div><!-- /row -->
-</section>
+                </div>
+                <div class="contact"> 
+                    <h2><?php the_field('h2'); ?></h2>
+                    <div class="wrapper">
+                    <img src="<?php bloginfo('template_directory') ?>/assets/images/address.png" alt="">
+                    <p><?php the_field('address'); ?></p>
+                    </div>
+                    <div class="wrapper">
+                    <img src="<?php bloginfo('template_directory') ?>/assets/images/email.png" alt="">
+                    <p><?php the_field('email'); ?></p>
+                    </div>
+                    <div class="wrapper">
+                    <img src="<?php bloginfo('template_directory') ?>/assets/images/phone.png" alt="">
+                    <p><?php the_field('phone'); ?></p>
+                    </div>
+                    <div class="wrapper socialmedia">
+                    <img src="<?php bloginfo('template_directory') ?>/assets/images/facebook.png" alt="">
+                    <img src="<?php bloginfo('template_directory') ?>/assets/images/facebook.png" alt="">
+                    <img src="<?php bloginfo('template_directory') ?>/assets/images/facebook.png" alt="">
+                    </div>
+                </div>
+        </section>
 <?php get_footer(); ?>
