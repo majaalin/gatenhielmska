3f76dcd8f24f02f3b0b38b95f3d0fdb4b37d3049
majaalin <maja_alin@hotmail.com>
2020-04-13 10:48:12 +0200

    Update javascript

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -27,7 +27,7 @@ if (homeScrollButton) {
 // On scroll change header color
 function scrollFunction() {
   const subMenu = document.querySelectorAll(".sub-menu");
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
     document.querySelector("header").style.backgroundColor =
       "rgba(13, 17, 23, 0.97)";
     subMenu.forEach(element => {
@@ -47,16 +47,8 @@ const hamburger = document.querySelector(".nav-icon");
 function open() {
   const menu = document.querySelector(".mobile-menu ");
   menu.classList.toggle("open");
-  // document.body.classList.toggle("overflow-hidden");
 }
 hamburger.addEventListener("click", open);
-// function openMenu() {
-//   const mobile = document.querySelector(".mobile");
-//   const mobilMenu = mobile.querySelectorAll(".sub-menu");
-//   mobilMenu.forEach(menu => {
-//     menu.classList.toggle("sub-menu-open");
-//   });
-//   console.log(mobilMenu);
 
 fetch("wp-json/wl/v1/instagram")
   .then(res => res.json())
