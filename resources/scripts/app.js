3c832e980870bad822e10ed35929754d08b5a82b
majaalin <maja_alin@hotmail.com>
2020-04-16 15:33:59 +0200

    Update javascript header

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -37,6 +37,7 @@ function scrollFunction() {
     });
   } else {
     document.querySelector("header").style.backgroundColor = "transparent";
+    document.querySelector(".mobile").style.backgroundColor = "transparent";
     subMenu.forEach(element => {
       element.style.backgroundColor = "transparent";
     });
